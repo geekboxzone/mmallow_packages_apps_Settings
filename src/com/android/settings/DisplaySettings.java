e432727c11235fa5015d3e6c36d05b6dad65db94
gouwa <geekbox@geekbuying.com>
2016-02-01 15:45:19 +0800

    Display: add the "Never" timeout option in sleep menu

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -277,8 +277,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                         best = i;
                     }
                 }
-                summary = preference.getContext().getString(R.string.screen_timeout_summary,
-                        entries[best]);
+                if (best+1 == values.length) {
+                    summary = preference.getContext().getString(R.string.screen_never_timeout_summary,
+                            entries[best]);
+                } else {
+                    summary = preference.getContext().getString(R.string.screen_timeout_summary,
+                            entries[best]);
+                }
             }
         }
         preference.setSummary(summary);
